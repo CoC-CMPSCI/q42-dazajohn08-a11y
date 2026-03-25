ebbdb9474029a3cfb784a34db650b5407d72948d
John <dazajohn08@gmail.com>
2026-03-24 23:28:38 -0700

    #1

--- main.cpp
+++ main.cpp
@@ -15,6 +15,26 @@ int main()
     cin >> weight >> distance;
 
     // TODO
+    if ( weight <= 0 || weight > 20 || distance < 10 || distance > 3000){
+        cout << "Wrong input" << endl;
+        return 1;
+    }
+
+    if (weight <= 2)
+    rate = rate1;
+    else if (weight <= 6)
+    rate = rate2;      
+    else if (weight <= 10)
+    rate = rate3;      
+    else
+    rate = rate4;
+
+    if (distance > 500)
+        total_charge = (distance / 500.0) * rate;
+    else
+        total_charge = rate;    
+    
+    
 
     cout << setw(10) << left << setprecision(2) << fixed;
     cout << total_charge << endl;
